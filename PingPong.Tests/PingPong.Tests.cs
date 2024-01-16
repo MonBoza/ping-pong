00e416be5088c4e582e2e169f50404cd659fb99e
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-01-16 16:15:29 -0600

    Test 3 pass Co-authored-by: Brianca Knight <briancaknight@gmail.com>

--- PingPong.Tests/PingPong.Tests.cs
+++ PingPong.Tests/PingPong.Tests.cs
@@ -21,6 +21,14 @@ namespace PingPong.Tests
     int result = newGame.Number;
     Assert.AreEqual(userNumber, result);
   }
-
+  [TestMethod]
+  public void SetUserNumber_SetValue_Void()
+  {
+  
+    Game newGame = new Game(5);
+    int newNumber = 10;
+    newGame.Number = newNumber;
+    Assert.AreEqual(newNumber, newGame.Number);
   }
+}
 }
--- ping-pong/Models/Game.cs
+++ ping-pong/Models/Game.cs
@@ -3,7 +3,9 @@ namespace PingPong.Models  // Namespace
 
   public class Game  // Class name
   {
-    public int Number { get; }
+     
+    public int Number { get; set;}
+    // private static List<Game> _instances = new List<Game> {};
     public Game(int userNumber)
     {
       Number = userNumber;
