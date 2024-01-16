b4846cfd1102292eb5cfcc1eba54f6863832d0c4
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-01-16 17:02:41 -0600

    Test 4 pass Co-authored-by: Brianca Knight <briancaknight@gmail.com>

--- PingPong.Tests/PingPong.Tests.cs
+++ PingPong.Tests/PingPong.Tests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PingPong.Models;
+using System.Collections.Generic;
+using System;
 
 namespace PingPong.Tests
 {
   [TestClass]
-  public class PingPongTests
+  public class PingPongTests : IDisposable
   {
+    public void Dispose()
+    {
+      Game.ClearAll();
+    }
+
     [TestMethod]
     public void GameConstructor_CreatesInstanceOfGame_Game()
     {
@@ -13,22 +20,33 @@ namespace PingPong.Tests
       Assert.AreEqual(typeof(Game), newGame.GetType());
     }
 
-  [TestMethod]
-  public void GetUserNumber_ReturnInt_Int()
-  {
-    int userNumber = 5;
-    Game newGame = new Game(userNumber);
-    int result = newGame.Number;
-    Assert.AreEqual(userNumber, result);
+    [TestMethod]
+    public void GetUserNumber_ReturnInt_Int()
+    {
+      int userNumber = 5;
+      Game newGame = new Game(userNumber);
+      int result = newGame.Number;
+      Assert.AreEqual(userNumber, result);
+    }
+    [TestMethod]
+    public void SetUserNumber_SetValue_Void()
+    {
+
+      Game newGame = new Game(5);
+      int newNumber = 10;
+      newGame.Number = newNumber;
+      Assert.AreEqual(newNumber, newGame.Number);
+    }
+
+    [TestMethod]
+
+    public void GetAll_ReturnNumberList_List()
+    {
+      int userNumber = 5;
+      Game game1 = new Game(userNumber);
+      List<int> expected = new List<int> { 1, 2, 3, 4, 5 };
+      List<int> actualResult = Game.CreateList(userNumber);
+      CollectionAssert.AreEqual(expected, actualResult);
+    }
   }
-  [TestMethod]
-  public void SetUserNumber_SetValue_Void()
-  {
-  
-    Game newGame = new Game(5);
-    int newNumber = 10;
-    newGame.Number = newNumber;
-    Assert.AreEqual(newNumber, newGame.Number);
-  }
-}
 }
--- ping-pong/Models/Game.cs
+++ ping-pong/Models/Game.cs
@@ -1,15 +1,36 @@
+using System.Collections.Generic;
+
 namespace PingPong.Models  // Namespace
 {
 
   public class Game  // Class name
   {
-     
-    public int Number { get; set;}
-    // private static List<Game> _instances = new List<Game> {};
+
+    public int Number { get; set; }
+    private static List<Game> _instances = new List<Game> { };
     public Game(int userNumber)
     {
       Number = userNumber;
-
     }
+
+    public static List<int> CreateList(int userInput)
+    {
+      List<int> gameList = new List<int>();
+      for (int index = 1; index <= userInput; index++)
+      {
+        gameList.Add(index);
+      }
+      return gameList;
+    }
+
+    public static List<Game> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
   }
 }
